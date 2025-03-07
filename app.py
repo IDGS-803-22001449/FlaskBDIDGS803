9f75506bffe2f7090d30d90ca769cbc9ba23fc83
IDGS-803-22001449 <81856@alumnos.utleon.edu.mx>
2025-03-06 22:34:35 -0600

    agregar

--- app.py
+++ app.py
@@ -19,7 +19,33 @@ def page_not_found(e):
 @app.route("/")
 @app.route("/index")
 def index():
-	return render_template("index.html")
+	create_form=forms.UserForm2(request.form)
+	alumno = Alumnos.query.all()# select * from alumnos
+	return render_template("index.html", form=create_form, alumnos=alumno)
+
+@app.route('/detalles', methods=['GET', 'POST'])
+def detalles():
+	create_form=forms.UserForm2(request.form)
+	if request.method == 'GET':
+		id=request.args.get('id')
+		# select * from alumnos where id=id
+		alum1 = db.session.query(Alumnos).filter(Alumnos.id==id).first()
+		nombre=alum1.nombre
+		apaterno=alum1.apaterno
+		email=alum1.email
+	return render_template('detalles.html', id=id, nombre=nombre, apaterno=apaterno, email=email)
+  
+  
+@app.route('/Alumnos1', methods=['GET','POST'])
+def Alumnos1():
+	create_form=forms.UserForm2(request.form)
+	if request.method == 'POST':
+		alum=Alumnos(nombre=create_form.nombre.data, apaterno=create_form.apaterno.data, email=create_form.email.data)
+		# insert into alumnos() values()
+		db.session.add(alum)
+		db.session.commit()
+		return redirect(url_for('index'))
+	render_template('Alumnos1.html', dorm=create_form)
 
 if __name__ == '__main__':
 	csrf.init_app(app)
